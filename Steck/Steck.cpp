2190d48b39048faf9b3e7a10bcec665b08e53b4b
Dima <Dima@DESKTOP-8CJJBIF>
2023-02-21 15:12:55 +0300

    Добавьте файлы проекта.

---
+++ Steck/Steck.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include "Stack.h"
+
+using namespace std;
+
+int main()
+{
+	Stack a;
+	Stack b;
+	Stack c;
+
+	for (int i = 0; i < 10; i++) {
+		a.push((char)rand()%100);
+	}
+
+	a.show();
+	cout << endl;
+	b.cloning(a);
+	cout << endl;
+	b.show();
+	cout << endl;
+	for (int i = 0; i < 10; i++) {
+		c.push((char)rand()%100);
+	}
+	c.show();
+	c.show();
+}
